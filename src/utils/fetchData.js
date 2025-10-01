da2f5adb837f774e8911533b8550606bed5b04fa
Uhut Sancar <sancaruhut@gmail.com>
2025-10-01 14:16:03 +0300

    searchexercises completed

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return null;
+  }
+};
