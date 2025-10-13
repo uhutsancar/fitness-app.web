945251de3e9bf7c500e820fccf56904ed1f7019d
Uhut Sancar <sancaruhut@gmail.com>
2025-10-13 14:06:18 +0300

    daily commit

---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,50 @@
+import { Box, Typography } from "@mui/material";
+import React, { useContext } from "react";
+import BodyPart from "./BodyPart";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="left-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+  return (
+    //hata buradakı box dan gelıyor olabılır 
+    <ScrollMenu
+      LeftArrow={LeftArrow}
+      RightArrow={RightArrow}
+      wrapperClassName="horizontal-scroll-wrapper"
+      scrollContainerClassName="horizontal-scroll-container"
+    >
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemID={item.id || item}
+          title={item.id || item}
+          className="scroll-item"
+          sx={{ m: "0 40px" }}
+        >
+          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+};
+
+export default HorizontalScrollbar;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,18 +6,45 @@ export const exerciseOptions = {
   },
 };
 
-export const fetchData = async (url, options) => {
-  try {
-    const response = await fetch(url, options);
-    
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Fetch error:", error);
-    return null;
-  }
+export const youtubeOptions = {
+  method: "GET",
+  url: "https://youtube-search-and-download.p.rapidapi.com/video/related",
+  headers: {
+    "X-RapidAPI-Key":  "259b83ee96mshd7e6274627677acp1b73b0jsne7e61cdff94a",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
 };
+
+
+
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
+
+
+// export const fetchData = async (url, options) => {
+//   try {
+//     const response = await fetch(url, options);
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Fetch error:", error);
+//     return null;
+//   }
+// };
+
+
+
+
+
+
+
